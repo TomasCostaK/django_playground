6ce409cf126afedf716ba3c7e0fa942544a5e0e9
tom1k <tomascosta@ua.pt>
2020-07-10 15:21:38 +0100

    Done URL Pathing and inc requests

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from pages.views import home_view, contact_view
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('', home_view, name='home'),
+    path('contact/', contact_view, name='contact'),
     path('admin/', admin.site.urls),
 ]
